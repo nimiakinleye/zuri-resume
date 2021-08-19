d6de4033526cabb04338d12b897be9ac674c0c4e
DESKTOP-IC0DK3R\Nimi <toluwanimiakinleye01@gmail.com>
2021-08-20 00:57:19 +0100

    reduced border radius

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ app.use((req, res) => {
   res.send('<h2>Page not found</h2>')
 })
 
-app.listen(1000)
+// app.listen(1000)
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
